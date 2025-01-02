4ea000beb52311389c00c7f8a8ca0bb1194e421c
Filipe Andrade <filipeyay@gmail.com>
2025-01-02 03:50:50 +0100

    add powershell config

---
+++ config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+# Theme
+oh-my-posh init pwsh --config 'C:\Users\USER\AppData\Local\Programs\oh-my-posh\themes\illusi0n.omp.json' | Invoke-Expression # change 'USER' with your username
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# Alias
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
