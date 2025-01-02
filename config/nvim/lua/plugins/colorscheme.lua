b0ed4da1049e2d591dedde7ef731a50e159af7a0
Filipe Andrade <filipeyay@gmail.com>
2025-01-02 03:48:38 +0100

    add monokai nightasty theme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,22 +1,13 @@
 return {
-  -- Cobalt2
   {
-    "lalitmee/cobalt2.nvim",
-    lazy = false,
-    priority = 1000,
-    dependencies = { "tjdevries/colorbuddy.nvim", tag = "v1.0.0" },
-    init = function()
-        require("colorbuddy").colorscheme("cobalt2")
-    end,
-  },
-
-  -- Kanagawa Paper
-  {
-    "sho-87/kanagawa-paper.nvim",
+    "polirritmico/monokai-nightasty.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      -- transparent = true,
-    },
+      hl_styles = {
+        keywords = { italic = false },
+        comments = { italic = false },
+      },
+    }
   }
 }
