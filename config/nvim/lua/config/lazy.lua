aa7295a6e6ca1ea9b2d2c278f14bec6eda8f43c6
Filipe Andrade <filipeyay@gmail.com>
2025-02-10 21:29:06 +0000

    add catppuccin theme

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
       opts = {
-        colorscheme = "monokai-nightasty",
+        colorscheme = "catppuccin-macchiato",
       },
     },
     { import = "plugins" },
