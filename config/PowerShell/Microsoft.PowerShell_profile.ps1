56eb1c58ecee9d25f5c0e36c02e8add02991eb49
Filipe Andrade <filipeyay@gmail.com>
2025-08-27 19:50:44 -0300

    updated powershell theme and added vi alias setting

--- config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 # Theme
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/gruvbox.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/emodipt.omp.json" | Invoke-Expression
 
 # Icons | run - Install-Module -Name Terminal-Icons -Repository PSGallery
 Import-Module -Name Terminal-Icons
@@ -9,4 +9,5 @@ Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
 Set-Alias vim nvim
+Set-Alias vi nvim
 Set-Alias whereis where.exe
